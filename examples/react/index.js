a8ea8ba8fcb1925b4c5afd7ca7001f8ac6906ab1
Luis Antunes <luisantunes.developer@gmail.com>
2019-03-16 02:35:11 +0000

    some tweaks on react example

--- examples/react/index.js
+++ examples/react/index.js
@@ -1,13 +1,43 @@
+/**
+ * The idea behind this example is to have a simple
+ * and descriptive state management, without too much
+ * boilerplate.
+ *
+ * A Web application can easily be handled with
+ * Meanly machines which outputs on transitions.
+ *
+ * In this case every time a click is made a
+ * transition is triggered calling the listeners
+ * at the end of the transition. That includes
+ * every before or after function.
+ */
+
 import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 const StateMachine = require('../../src/index');
 
+// the Services user mock
 const user = {
   name: 'John',
   surname: 'Doe',
 };
 
+class Services {
+  async getUser() {
+    // simulates an api call
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(user);
+      }, 1000);
+    });
+  }
+}
+
+// configure the state machine
+// it's ok, to configure the machine up front
+// with all the listeners, but this could be
+// done afterwards
 const getConfig = beforeSecondState => ({
   firstState: {
     secondState: {
@@ -17,31 +47,19 @@ const getConfig = beforeSecondState => ({
   secondState: {},
 });
 
-class Services {
-  async getUser() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(user);
-      }, 1000);
-    });
-  }
-}
-
+// create a new state machine
+// with states, initial state and
+// state changes observers
 const sm = new StateMachine();
 const states = getConfig(new Services().getUser);
 sm.config(states)('firstState');
 
 class SMExample extends React.Component {
-  state;
-  constructor(props) {
-    super(props);
-    this.state = sm.getState() || { data: {} };
-  }
-
   componentDidMount() {
-    sm.init().attach('secondState', 'SMExample', data => {
-      this.setState({ data });
-    });
+    // first attach the listeners and then start the state machine
+    sm.attach('firstState', 'SMExample', () => this.setState({ data: {} }))
+      .attach('secondState', 'SMExample', data => this.setState({ data }))
+      .init();
   }
 
   componentWillUnmount() {
@@ -49,10 +67,19 @@ class SMExample extends React.Component {
   }
 
   render() {
+    // the important part is the transitions
+    // a state machine changes state on a certain event
+    // who decides when is up to the user
+    // in this case the two onClicks below
     return (
       <div>
+        <p>{sm.getState()}</p>
         <button onClick={() => sm.transition('secondState')}> Click</button>
-        {this.state.data && this.state.data.name && (
+        <span> </span>
+        {sm.getState() !== 'firstState' && (
+          <button onClick={() => sm.transition('firstState')}> Back</button>
+        )}
+        {this.state && this.state.data && this.state.data.name && (
           <p>{this.state.data.name}</p>
         )}
       </div>

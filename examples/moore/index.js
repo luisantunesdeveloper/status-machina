44e8ad593f67f1a73acb614efcda857001d36d8c
Luis Antunes <luisantunes.developer@gmail.com>
2019-03-20 17:39:17 +0000

    Fix #5 issue: add moore state machine config; separate tests by generic, moore; add state machine types;

--- examples/moore/index.js
+++ examples/moore/index.js
@@ -8,52 +8,60 @@
 
 const StateMachine = require('../../src/index');
 
-const action = data =>
+const outputFn0 = data =>
   new Promise(resolve => {
-    setTimeout(() => {
-      data.inc = ++data.inc;
-      resolve(data);
-    }, 0);
+    resolve(data + '0');
   });
 
-// configure the state machine
-const getConfig = () => ({
+const outputFn1 = data =>
+  new Promise(resolve => {
+    resolve(data + '1');
+  });
+
+const config = {
+  // initial: 's0',
+  // inputs: ['0', '1'],
   s0: {
+    on: {
+      outputs: outputFn0,
+    },
     s1: {},
   },
   s1: {
     on: {
-      actions: [action],
+      outputs: outputFn0,
+    },
+    s1: {},
+    s2: {},
+  },
+  s2: {
+    on: {
+      outputs: outputFn1,
     },
   },
-});
+};
 
 new Promise(resolve => {
   // create a new state machine
   // with states, initial state and
   // state changes observers
   const sm = new StateMachine();
-  const states = getConfig();
-  sm.config(states)('s0')
-    .attach('s0', 'observerId', () =>
-      console.log(
-        `machine is on ${
-          sm.currentState
-        } with current output of ${JSON.stringify(sm.data)}`
-      )
-    )
-    .attach('s1', 'observerId', () =>
-      console.log(
-        `machine is on ${
-          sm.currentState
-        } with current output of ${JSON.stringify(sm.data)}`
-      )
-    )
-    .init({ inc: 0 });
+  sm.configMoore(config)('s0').init('');
   resolve(sm);
 }).then(async sm => {
   try {
     await sm.transition('s1');
+    console.log(
+      `machine is on ${
+        sm.currentState
+      } state with current output of ${JSON.stringify(sm.data)}`
+    );
+    await sm.transition('s2');
+    console.log(
+      `machine is on ${
+        sm.currentState
+      } state with current output of ${JSON.stringify(sm.data)}`
+    );
   } catch (error) {
     console.log(error);
   }
